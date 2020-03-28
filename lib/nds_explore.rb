00ec8a36ae6316d18922233d0a4879ff544ee140
Alice Wang <aywang6@gmail.com>
2020-03-28 02:27:18 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,9 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  puts "XXXXXXXXXXX" pp directors_database
+  puts "XXXXXXXXXXX" 
   puts pp directors_database
-  puts "XXXXXXXXXXX" pp directors_database
+  puts "XXXXXXXXXXX" 
 end
 
 def print_first_directors_movie_titles
